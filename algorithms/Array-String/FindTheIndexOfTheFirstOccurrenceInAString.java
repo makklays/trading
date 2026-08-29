59e98b8b822e867c3b71f9226dbcbe30d0aeeacb
Alexander Kuziv <makklays@gmail.com>
2026-08-29 09:15:49 +0200

    Add strStr method to find substring index
    Implement strStr method to find the first occurrence of a substring.

---
+++ algorithms/Array-String/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,14 @@
+/**
+ * Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ *
+ * @author Alexander Kuziv <makklays@gmail.com>
+ */
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int index = haystack.indexOf(needle);
+
+        return index;
+    }
+}
+
