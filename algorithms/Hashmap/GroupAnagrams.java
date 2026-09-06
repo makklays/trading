e88fcd54e845542f74260d21a6caf80cf689833b
Alexander Kuziv <makklays@gmail.com>
2026-09-06 19:40:21 +0200

    Update GroupAnagrams.java

--- algorithms/Hashmap/GroupAnagrams.java
+++ algorithms/Hashmap/GroupAnagrams.java
@@ -6,7 +6,30 @@
 
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        //
+        // Если массив пустой, возвращаем пустой список 
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+
+        // Карта для группировки, ключ - слово отсортированное по алфавиту, значение - список анаграмм 
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            // Получаем отсортированную строку, которая служит уникальным ключом 
+            String key = String.valueOf(chars);
+
+            // Если такого ключа еще нет, создаем для него новый список 
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+
+            map.get(key).add(s);
+        }
+
+        // Возвращаем списки отсортированных слов 
+        return new ArrayList<>(map.values());
     }
 }
 
