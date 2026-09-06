b5c64253e753f2580c45354f5f6077b5000da6ff
Alexander Kuziv <makklays@gmail.com>
2026-09-06 19:17:51 +0200

    Create GroupAnagrams.java

---
+++ algorithms/Hashmap/GroupAnagrams.java
@@ -0,0 +1,12 @@
+/**
+ * Given an array of strings strs, group the together. You can return the answer in any order.
+ *
+ * @author Alexander Kuziv <makklays@gmail.com>
+ */ 
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        //
+    }
+}
+
