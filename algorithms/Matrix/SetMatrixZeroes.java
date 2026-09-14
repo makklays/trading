9fe6c3f4b8e54449ef65d09479cdf1c40a8c5d7e
Alexander Kuziv <makklays@gmail.com>
2026-09-14 22:41:08 +0200

    Update SetMatrixZeroes.java

--- algorithms/Matrix/SetMatrixZeroes.java
+++ algorithms/Matrix/SetMatrixZeroes.java
@@ -1,6 +1,7 @@
 /**
  * Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
  * You must do it in place.
+ * (in place), что означает, что вам нужно изменить саму входную матрицу напрямую (без создания новой)
  * 
  * Example 1:
  * Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
