4c390a6148ddedc078723fc510e5381816f352b6
Alexander Kuziv <makklays@gmail.com>
2026-09-14 22:18:45 +0200

    Update RotateImage.java

--- algorithms/Matrix/RotateImage.java
+++ algorithms/Matrix/RotateImage.java
@@ -6,8 +6,8 @@
  * Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
  * Output: [[7,4,1],[8,5,2],[9,6,3]]
  *
- * Time Complexity: O(n)
- * Space Complexity: O(n) 
+ * Time Complexity: O(n^2)
+ * Space Complexity: O(1) 
  * 
  * @author Alexander Kuziv <makklays@gmail.com>
  */ 
